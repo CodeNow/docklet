4292305ba594bc873c854ab9084d7c612370e65b
generalhenry <htilford@gmail.com>
2013-09-12 17:20:12 -0700

    fab wip

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,38 @@
 TODO: Turn this into a fab file
 
+def prep_for_docker:
+  
+
+def install_docker:
+  sudo('apt-get install -y linux-image-extra-`uname -r` curl')
+  sudo('curl https://get.docker.io/ubuntu/info | sh -x')
+
+def install_gvm:
+  sudo('apt-get install -y git mercurial bison')
+  run('bash < <(curl -s https://raw.github.com/moovweb/gvm/master/binscripts/gvm-installer)')
+
+def install_go:
+  install_gvm()
+  run('gvm install go1.1.2')
+
+def install_etcd:
+  sudo('apt-get install -y git')
+  run('git clone https://github.com/coreos/etcd')
+  with cd('etcd'):
+    run('git checkout v0.1.1')
+    run('gvm use go1.1.2 && ./build')
+
+def start_etcd:
+  run('killall etcd')
+  run('cd etcd && nohup ./etcd -s {LOCAL_IP}:7001 -c {LOCAL_IP}:4001 -C 10.0.1.226:7001 -d nodes/node1 -n docklet{DOCKLET_INDEX}')
+
+def install_node:
+  sudo('apt-get update')
+  sudo('apt-get install -y python-software-properties python g++ make')
+  sudo('sudo add-apt-repository ppa:chris-lea/node.js')
+  sudo('apt-get update')
+  sudo('apt-get install nodejs')
+
 wget https://go.googlecode.com/files/go1.1.2.linux-amd64.tar.gz
 tar -xvf go1.1.2.linux-amd64.tar.gz
 echo "export GOROOT=\$HOME/go" >> ~/.bash_profile
