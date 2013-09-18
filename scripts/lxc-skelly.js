76813467ad3cd4d8f6355aff2954f22a0ea46ba8
generalhenry <htilford@gmail.com>
2013-09-17 18:30:26 -0700

    holy water

---
+++ scripts/lxc-skelly.js
@@ -0,0 +1,60 @@
+var async = require('async');
+var configs = require('../lib/configs');
+var docker = require('docker.js')({
+  host: 'http://' + configs.docker_host + ':' + configs.docker_port
+});
+
+
+findSkeletons();
+
+
+
+function findSkeletons () {
+  docker.listContainers(filter);
+}
+
+function filter (err, containers) {
+  if (err) {
+    throw err;
+  }
+  console.log('LIST', containers.map(id));
+  async.filterSeries(containers, top, applyHolyWater)
+}
+
+function id (container) {
+  return container.Id;
+}
+
+function top (container, cb) {
+  docker.listProcesses(container.Id, check);
+
+  function check (err, processes) {
+    if (err) {
+      throw err;
+    } 
+    console.log('PROCESSESS', container.Id, processes.Processes === null);
+    cb(processes.Processes === null);
+  }
+}
+
+function applyHolyWater (containers) {
+  console.log('SKELETONS', containers.map(id));
+  async.eachSeries(containers, kill, celebrate);
+}
+
+function kill (container, cb) {
+  docker.killContainer(container.Id, dead);
+  function dead (err) {
+    console.log('DEAD', container.Id);
+    cb(err);
+  }
+}
+
+function celebrate (err) {
+  if (err) {
+    throw err;
+  }
+  console.log('cleaned dem skeletons');
+  setTimeout(findSkeletons, 5000);
+}
+
